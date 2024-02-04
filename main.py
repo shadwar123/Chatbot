3289a5bc7ca45c564b4d3877ad19b616c29d8740
shadwar123 <shadwarnayyar@gmail.com>
2024-02-04 06:36:34 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import os
+
 import streamlit as st
 from dotenv import load_dotenv
 import google.generativeai as gen_ai
-import pyttsx3
+
 
 # Load environment variables
 load_dotenv()
@@ -20,8 +21,6 @@ GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
 gen_ai.configure(api_key=GOOGLE_API_KEY)
 model = gen_ai.GenerativeModel('gemini-pro')
 
-# Initialize text-to-speech engine
-engine = pyttsx3.init()
 
 # Function to translate roles between Gemini-Pro and Streamlit terminology
 def translate_role_for_streamlit(user_role):
@@ -30,9 +29,11 @@ def translate_role_for_streamlit(user_role):
     else:
         return user_role
 
+
 # Initialize chat session in Streamlit if not already present
 if "chat_session" not in st.session_state:
     st.session_state.chat_session = model.start_chat(history=[])
+
 
 # Display the chatbot's title on the page
 st.title("🤖 Gemini Pro - ChatBot")
@@ -41,19 +42,12 @@ st.title("🤖 Gemini Pro - ChatBot")
 for message in st.session_state.chat_session.history:
     with st.chat_message(translate_role_for_streamlit(message.role)):
         st.markdown(message.parts[0].text)
-        # Add voice output for each message
-        engine.say(message.parts[0].text)
-
 
 # Input field for user's message
 user_prompt = st.chat_input("Ask Gemini-Pro...")
-
 if user_prompt:
     # Add user's message to chat and display it
     st.chat_message("user").markdown(user_prompt)
-    # Add voice output for user's message
-    engine.say(user_prompt)
-
 
     # Send user's message to Gemini-Pro and get the response
     gemini_response = st.session_state.chat_session.send_message(user_prompt)
@@ -61,6 +55,3 @@ if user_prompt:
     # Display Gemini-Pro's response
     with st.chat_message("assistant"):
         st.markdown(gemini_response.text)
-        # Add voice output for Gemini-Pro's response
-        engine.say(gemini_response.text)
-        engine.runAndWait()
